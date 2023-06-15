b5c4ace777c4a00ab277a5527318affa529cd1db
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2023-06-15 11:02:58 -0300

    :lipstick: feat create DownArraw Component

---
+++ pokedex/src/components/icons/DownArraw.jsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+
+export function DownArrawIcon(props) {
+  return (
+    <Svg
+      width={9}
+      height={16}
+      viewBox="0 0 9 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <Path
+        d="M9.017 11.108l-.252-.252a.429.429 0 00-.606 0l-2.98 2.997V.43A.429.429 0 004.75 0h-.357a.429.429 0 00-.429.429v13.424l-2.98-2.997a.429.429 0 00-.606 0l-.252.252a.429.429 0 000 .606l4.142 4.16a.429.429 0 00.607 0l4.142-4.16a.429.429 0 000-.606z"
+        fill="#212121"
+      />
+    </Svg>
+  )
+}
