de76b5a7d2a6dabaf3e26ee601870629c6f72c34
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2023-06-15 11:04:16 -0300

    :lipstick: feat create index.tsx for global imports

---
+++ pokedex/src/components/index.tsx
@@ -0,0 +1,30 @@
+import { SearchBar } from "./searchBar";
+
+import { ListPokemons } from "./list";
+
+import { DetailsType } from "./detailsType";
+import { DetailsNameBaseStats } from "./detailsNameBaseStats";
+import { DetailsNumberBaseStats } from "./detailsNumberBaseStats";
+
+import { PokeballIcon } from "./icons/Pokeball";
+import { WeightIcon } from "./icons/Weight";
+import { HeightIcon } from "./icons/Height";
+import { LeftArrawIcon } from './icons/LeftArraw';
+import { LeftIcon } from './icons/Left';
+import { RightIcon } from './icons/Right';
+import { DownArrawIcon } from './icons/DownArraw';
+
+export {
+    SearchBar,
+    ListPokemons,
+    DetailsType,
+    PokeballIcon,
+    WeightIcon,
+    HeightIcon,
+    LeftArrawIcon,
+    LeftIcon,
+    RightIcon,
+    DownArrawIcon,
+    DetailsNameBaseStats,
+    DetailsNumberBaseStats
+};
