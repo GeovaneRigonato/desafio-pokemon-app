0ffd769e0d2292cb5ee1c6ecd313b3051e3ad91d
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2023-06-15 11:03:07 -0300

    :lipstick: feat create Height Component

---
+++ pokedex/src/components/icons/Height.jsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+
+export function HeightIcon(props) {
+  return (
+    <Svg
+      width={9}
+      height={17}
+      viewBox="0 0 9 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <Path
+        d="M7.453.08h-6a1 1 0 00-1 1v14a1 1 0 001 1h6a1 1 0 001-1v-14a1 1 0 00-1-1zm-6 15v-14h6v2h-1.75a.25.25 0 00-.25.25v.5c0 .139.111.25.25.25h1.75v2h-1.75a.25.25 0 00-.25.25v.5c0 .139.111.25.25.25h1.75v2h-1.75a.25.25 0 00-.25.25v.5c0 .139.111.25.25.25h1.75v2h-1.75a.25.25 0 00-.25.25v.5c0 .139.111.25.25.25h1.75v2h-6z"
+        fill="#212121"
+      />
+    </Svg>
+  )
+}
