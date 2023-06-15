b711405369dcb55f9a0187dda4fc799e0f5b92dd
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2023-06-15 11:03:23 -0300

    :lipstick: feat create LeftArraw Component

---
+++ pokedex/src/components/icons/LeftArraw.jsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+
+export function LeftArrawIcon(props) {
+  return (
+    <Svg
+      width={19}
+      height={18}
+      viewBox="0 0 19 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <Path
+        d="M9.683 17.592l.773-.773a.469.469 0 000-.663l-6.06-6.06h13.338c.258 0 .468-.21.468-.469V8.534a.469.469 0 00-.468-.47H4.396l6.06-6.059a.469.469 0 000-.663L9.683.57a.469.469 0 00-.663 0L.84 8.749a.469.469 0 000 .663l8.18 8.18c.183.183.48.183.663 0z"
+        fill="#fff"
+      />
+    </Svg>
+  )
+}
