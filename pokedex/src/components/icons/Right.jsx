7043a32038471b117d0c449e5e11d0dd7f27a66f
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2023-06-15 11:03:38 -0300

    :lipstick: feat create Right Component

---
+++ pokedex/src/components/icons/Right.jsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+
+export function RightIcon(props) {
+  return (
+    <Svg
+      width={18}
+      height={18}
+      viewBox="0 0 9 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <Path
+        d="M1.225.271L.605.89a.375.375 0 000 .53l5.648 5.66-5.647 5.66a.375.375 0 000 .531l.619.619a.375.375 0 00.53 0l6.544-6.544a.375.375 0 000-.53L1.755.27a.375.375 0 00-.53 0z"
+        fill="#ffffff"
+      />
+    </Svg>
+  )
+}
