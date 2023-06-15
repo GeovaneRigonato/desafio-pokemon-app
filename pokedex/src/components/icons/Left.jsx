fe716477fdbaa358990f84c7b212209307af6541
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2023-06-15 11:03:14 -0300

    :lipstick: feat create Left Component

---
+++ pokedex/src/components/icons/Left.jsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+
+export function LeftIcon(props) {
+  return (
+    <Svg
+    width={18}
+    height={18}
+      viewBox="0 0 9 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <Path
+        d="M7.68 13.89l.62-.619a.375.375 0 000-.53L2.651 7.08 8.3 1.42a.375.375 0 000-.531L7.68.27a.375.375 0 00-.53 0L.606 6.815a.375.375 0 000 .53L7.15 13.89a.375.375 0 00.53 0z"
+        fill="#ffffff"
+      />
+    </Svg>
+  )
+}
